a660839118e6838d9a581fcaf9fb5467067c8339
Jason Braun <jason@braundesign.co>
2019-06-06 21:50:53 -0700

    Added changed files

--- app/code/Cryozonic/StripePayments/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-/*jshint browser:true jquery:true*/
-/*global alert*/
-var config = {
-    map: {
-        '*': {
-            'cryozonic_stripe': 'Cryozonic_StripePayments/js/cryozonic_stripe'
-        }
-    }
-};
--- app/code/Klaviyo/Reclaim/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Klaviyo_Reclaim',
-    __DIR__
-);
